9249b26a8007f2b8a2155c2fb5518fbd1a14ef30
evelinasadurska <eve.sadurska@gmail.com>
2026-06-08 01:01:49 +0300

    Started home-page

--- sniff-and-stroll/resources/js/app.js
+++ sniff-and-stroll/resources/js/app.js
@@ -5,3 +5,17 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+
+window.addEventListener('load', () => {
+    AOS.init({
+        duration:1000,
+        offset:120,
+        once: true,
+    });
+
+    AOS.refresh();
+});
