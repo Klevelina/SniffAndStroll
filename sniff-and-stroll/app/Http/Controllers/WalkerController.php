6d4c0b4cc50d31c18cc4e1c7a1c53953c78a7356
Clint Le Grange <sentinal042@gmail.com>
2026-06-12 23:17:33 +0300

    Implemented the Walker Dashboard

---
+++ sniff-and-stroll/app/Http/Controllers/WalkerController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\WalkSession;
+
+use Illuminate\Http\Request;
+
+class WalkerController extends Controller
+{
+    public function dashboard()
+    {
+        $walks = auth()->user()
+            ->walkingSessions()
+            ->with(['dog', 'owner'])
+            ->latest()
+            ->get();
+
+        return view(
+            'walker.dashboard',
+            compact('walks')
+        );
+    }
+
+    public function accept(WalkSession $walkSession)
+    {
+        if ($walkSession->walker_id !== auth()->id()) {
+            abort(403);
+        }
+
+        $walkSession->update([
+            'status' => 'accepted'
+        ]);
+
+        return redirect()
+            ->route('walker.dashboard');
+    }
+}
--- sniff-and-stroll/routes/web.php
+++ sniff-and-stroll/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\DogController;
 use App\Http\Controllers\WalkSessionController;
+use App\Http\Controllers\WalkerController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,12 +14,12 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/walkers', function () {
+Route::get('/walker', function () {
 
     $walkers = \App\Models\User::where('role', 'walker')
         ->get();
 
-    return view('walkers.index', compact('walkers'));
+    return view('walker.index', compact('walkers'));
 });
 
 Route::get('/walker/dashboard', function () {
@@ -35,11 +36,13 @@ Route::get('/walker/dashboard', function () {
 });
 
 Route::middleware('auth')->group(function () {
+    Route::get('/walker/dashboard', [WalkerController::class, 'dashboard'])->name('walker.dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::resource('dogs', DogController::class);
     Route::resource('walk-sessions', WalkSessionController::class);
+    Route::patch('/walk-sessions/{walkSession}/accept', [WalkerController::class, 'accept'])->name('walk-sessions.accept');
 });
 
 require __DIR__.'/auth.php';
