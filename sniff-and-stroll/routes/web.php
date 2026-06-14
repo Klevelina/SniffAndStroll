386666902dea6c0becd8a02a869c98f33781a4ff
Clint Le Grange <sentinal042@gmail.com>
2026-06-14 09:28:17 +0300

    Implemented the remaining walk statuses, based on walker interaction

--- sniff-and-stroll/routes/web.php
+++ sniff-and-stroll/routes/web.php
@@ -15,6 +15,10 @@ Route::get('/about', function () {
     return view('about');
 })->name('about');
 
+Route::get('/contact',function() {
+    return view('contact');
+})->name('contact');
+
 /*
 |--------------------------------------------------------------------------
 | Admin Dashboard
@@ -73,15 +77,11 @@ Route::middleware('auth')->group(function () {
     Route::resource('walk-sessions', WalkSessionController::class);
 
     // Walker actions
-    Route::patch('/walk-sessions/{walkSession}/accept', [WalkerController::class, 'accept'])
-        ->name('walk-sessions.accept');
+    Route::patch('/walk-sessions/{walkSession}/accept', [WalkerController::class, 'accept'])->name('walk-sessions.accept');
+    Route::patch('/walk-sessions/{walkSession}/start', [WalkerController::class, 'start'])->name('walk-sessions.start');
+    Route::patch('/walk-sessions/{walkSession}/complete', [WalkerController::class, 'complete'])->name('walk-sessions.complete');
+    Route::patch('/walk-sessions/{walkSession}/decline', [WalkerController::class, 'decline'])->name('walk-sessions.decline');
 
-Route::get('/contact',function() {
-    return view('contact');
-})->name('contact');
-
-    Route::patch('/walk-sessions/{walkSession}/decline', [WalkerController::class, 'decline'])
-        ->name('walk-sessions.decline');
 });
 
 /*
